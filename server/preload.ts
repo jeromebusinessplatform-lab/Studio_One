d2c1d5a365fe4494c93250cea1fb81bc01561390
CODESPACE <jerome.businessplatform@gmail.com>
2026-08-23 17:19:24 +0800

    feat: install admin comparison metadata routes

--- server/preload.ts
+++ server/preload.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { installReleaseRoutes } from "./releaseRoutes.js";
 import { installProductComparisonRoutes } from "./productComparisonRoutes.js";
 import { installTelegramAvatarRoutes } from "./telegramAvatarRoutes.js";
+import { installAdminComparisonRoutes } from "./adminComparisonRoutes.js";
 
 const proto: any = (express.application as any);
 const originalListen = proto.listen;
@@ -11,6 +12,7 @@ if (!(proto as any).__primeReleaseRoutesInstalled) {
     installReleaseRoutes(this);
     installProductComparisonRoutes(this);
     installTelegramAvatarRoutes(this);
+    installAdminComparisonRoutes(this);
     return originalListen.apply(this, args);
   };
 }
